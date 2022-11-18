f8a091a66dd3520f4df808fd140322f642320e0e
Emily Davis <demily@vmware.com>
2022-11-18 16:25:40 -0500

    add spring initializr app and create react app

---
+++ src/main/java/com/example/stuffmonolith/StuffMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.example.stuffmonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuffMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuffMonolithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stuffmonolith/StuffMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stuffmonolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuffMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
