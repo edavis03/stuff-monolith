f02d84cfc3da90bc178c1bf2bfe7901ea5a4be10
Emily Davis <demily@vmware.com>
2022-11-19 10:05:33 -0500

    Add msw with config and initial handler

---
+++ frontend/src/mocks/handlers.ts
@@ -0,0 +1,13 @@
+// src/mocks/handlers.js
+import { rest } from 'msw'
+
+export const handlers = [
+    rest.get('/stuff',(req,res, ctx) => {
+        return res(
+            ctx.status(200),
+            ctx.json({
+                data: ["lamp", "sweatshirt", "kakapo"]
+            }),
+        )
+    }),
+]
